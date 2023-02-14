ffb2f7d2bdfb77f8518c733df880e1e499c6e592
Jesus Ramos <jeragi@floridauniversitaria.es>
2023-02-14 15:12:53 +0100

    añadiendo a atotal.sh

---
+++ atotal.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ciudad=$1
+
+while true; do
+  case $ciudad in
+    Valencia)
+      consumo=$(awk '/Valencia/ {sum+=$3} END {print sum}' consumo.txt)
+      echo "El consumo total de $ciudad es: $consumo"
+      break
+      ;;
+    Barcelona)
+      consumo=$(awk '/Barcelona/ {sum+=$3} END {print sum}' consumo.txt)
+      echo "El consumo total de $ciudad es: $consumo"
+      break
+      ;;
+    Madrid)
+      consumo=$(awk '/Madrid/ {sum+=$3} END {print sum}' consumo.txt)
+      echo "El consumo total de $ciudad es: $consumo"
+      break
+      ;;
+    *)
+      echo "Ciudad no encontrada. Introduce una ciudad válida: "
+      read ciudad
+      ;;
+  esac
+done
